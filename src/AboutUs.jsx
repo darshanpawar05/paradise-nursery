a47fb5267ebfa171afdadd87bfbf19798750d639
Darshan Pawar <darshan.pawar23@gmail.com>
2026-04-18 23:06:38 +0530

    fixed AboutUs

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -1,8 +1,24 @@
 export default function AboutUs() {
   return (
-    <div>
+    <div className="about-us-container">
       <h1>About Paradise Nursery</h1>
-      <p>We sell beautiful plants for your home.</p>
+
+      <p>
+        Welcome to Paradise Nursery! We are passionate about bringing nature
+        closer to your home. Our nursery offers a wide variety of indoor and
+        outdoor plants that help create a peaceful and refreshing environment.
+      </p>
+
+      <p>
+        Our mission is to provide high-quality plants at affordable prices while
+        promoting a greener and healthier lifestyle. Whether you are a beginner
+        or a plant lover, we have something for everyone.
+      </p>
+
+      <p>
+        We believe plants make life better. Explore our collection and start your
+        green journey with us today!
+      </p>
     </div>
   );
 }
